80a2663d81aafa68087cd7e77af7020d689dca5d
luiz-davi <luizd4398@gmail.com>
2022-12-12 13:57:39 -0300

    initial config

---
+++ src/server/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const server = express();
+
+
+server.get('/', (_, res) => {
+  return res.status(200).json({
+    message: "teste node"
+  })
+});
+
+
+export { server };
